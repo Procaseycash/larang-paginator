09010d79c0477382da68b5619668cd3a05f30521
KazeemO <kezyolanipekun@gmail.com>
2018-01-08 14:51:42 +0100

    readme updated

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,3 @@
-export * from './src/app/paginator/index';
+export {LarangPaginatorModule} from './src/app/paginator/larang.paginator.module';
+export {EventsService} from './src/app/paginator/event.service';
+export {PaginatorService} from './src/app/paginator/paginator.service';
--- src/app/paginator/index.ts
+++ src/app/paginator/index.ts
@@ -1,3 +1,3 @@
-export * from './larang.paginator.module';
+export {LarangPaginatorModule} from './larang.paginator.module';
 export {EventsService} from './event.service';
 export {PaginatorService} from './paginator.service';
